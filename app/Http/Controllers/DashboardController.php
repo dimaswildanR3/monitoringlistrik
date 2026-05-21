419f895801feeb001b80a7c51823c3216d1c4f25
dimaswildan28 <dimaswildan1986@gmail.com>
2026-05-22 01:17:16 +0700

    push full

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,12 +10,12 @@ class DashboardController extends Controller
 {
     public function index()
 {
-    $data = LogEnergy::latest()->limit(20)->get();
+    $data = LogEnergy::latest()->get();
     return view('dashboard', compact('data'));
 }
     public function hasil()
 {
-    $data = LogEnergy::latest()->limit(20)->get();
+    $data = LogEnergy::latest()->get();
     return view('hasil', compact('data'));
 }
 }
