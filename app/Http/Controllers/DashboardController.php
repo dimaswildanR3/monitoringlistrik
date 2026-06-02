164256b6b9e80a78d504bbf89f98f8ddd7e264c5
dimaswildan28 <dimaswildan1986@gmail.com>
2026-06-02 14:23:14 +0700

    git push

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,46 +4,31 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\LogEnergy;
-
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request)
-{
-    $query = LogEnergy::query();
+    public function index()
+    {
+        // auto delete data lebih dari 2 hari
+        LogEnergy::where('waktu_log', '<', now()->subDays(2))->delete();
 
-    if ($request->start && $request->end) {
-
-        // batas maksimal 2 hari
-        $start = \Carbon\Carbon::parse($request->start);
-        $end = \Carbon\Carbon::parse($request->end);
-
-        if ($start->diffInDays($end) > 2) {
-            $end = (clone $start)->addDays(2);
-        }
-
-        $query->whereBetween('waktu_log', [$start, $end]);
-
-    } else {
-
-        // DEFAULT: 2 hari terakhir (hari ini + kemarin)
-        $query->whereBetween('waktu_log', [
+        // ambil hanya 2 hari terakhir
+        $data = LogEnergy::whereBetween('waktu_log', [
             now()->subDays(1)->startOfDay(),
             now()->endOfDay()
-        ]);
+        ])->latest()->get();
+
+        return view('dashboard', compact('data'));
     }
 
-    $data = $query->latest()->get();
+    public function hasil()
+    {
+        $data = LogEnergy::whereBetween('waktu_log', [
+            now()->subDays(1)->startOfDay(),
+            now()->endOfDay()
+        ])->latest()->get();
 
-    return view('dashboard', compact('data'));
-}
-public function hasil()
-{
-    $data = LogEnergy::whereBetween('waktu_log', [
-        now()->subDays(1)->startOfDay(),
-        now()->endOfDay()
-    ])->latest()->get();
-
-    return view('hasil', compact('data'));
-}
+        return view('hasil', compact('data'));
+    }
 }
